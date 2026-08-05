e09bee27310e38d241e00bb044da7ea7f559aeac
huynd <huy.ngo@flashtech.asia>
2026-08-05 09:39:33 +0700

    fix(ui): use ZaloIcon.jpg public assets path for ZaloIcon component

--- packages/ui/src/components/icon-component/ZaloIcon.tsx
+++ packages/ui/src/components/icon-component/ZaloIcon.tsx
@@ -1,37 +1,26 @@
-import type { SVGProps } from "react";
+import type React from "react";
+
+export interface ZaloIconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  className?: string;
+  width?: number;
+  height?: number;
+}
 
 export function ZaloIcon({
-  className = "w-4 h-4 mr-2 shrink-0",
+  className = "w-4 h-4 mr-2 shrink-0 inline-block object-contain",
+  width = 16,
+  height = 16,
+  alt = "Zalo Icon",
   ...props
-}: SVGProps<SVGSVGElement>) {
+}: ZaloIconProps) {
   return (
-    <svg
+    <img
+      src="/assets/icons/social/ZaloIcon.jpg"
+      alt={alt}
+      width={width}
+      height={height}
       className={className}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
       {...props}
-    >
-      <title>Zalo Logo</title>
-      <rect width="24" height="24" rx="5" fill="#0068FF" />
-      <path
-        d="M5 8.5H11.5L7 15.5H13"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M14 11.5C14 10.1193 15.1193 9 16.5 9C17.8807 9 19 10.1193 19 11.5V13C19 14.3807 17.8807 15.5 16.5 15.5C15.1193 15.5 14 14.3807 14 13V11.5Z"
-        stroke="white"
-        strokeWidth="2"
-      />
-      <path
-        d="M20 8.5V15.5"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-    </svg>
+    />
   );
 }
